efef3065f3d606a189a7fe67f48a190a130c6023
Hyunjung Lee <hyunjung20222@gmail.com>
2023-02-11 15:23:08 +0900

    app.py 홈 라우트 추가

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
+
 @app.route("/")
 def main():
-    return "Welcome!"
+    return render_template('signin.html')
+
 if __name__ == "__main__":
-    app.run()
+    app.run(host = "0.0.0.0", port = 5000, debug = True)
